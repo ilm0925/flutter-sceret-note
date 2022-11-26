f66605c6af3e4d4a51027e376fca57dd49bf65de
leejw05 <zeta050525@gmail.com>
2022-11-26 09:29:59 +0900

    showDialog 만듬 ㅋ

--- README.md
+++ README.md
@@ -1,16 +1,4 @@
-# secret_note
+# 비밀노트(Todo list?)
 
-A new Flutter project.
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+- [ ] 암호 인증 만들기 (Navigator로)
+- [ ] 카드 클릭하면 상세보기로 모든 내용 보여주기
--- lib/main.dart
+++ lib/main.dart
@@ -26,67 +26,18 @@ class _MyAppState extends State<MyApp> {
     RulesMap(DateTime.now(), "규칙 1", 5),
     RulesMap(DateTime.now(), "규칙 2", 5),
     RulesMap(DateTime.now(), "규칙 3", 5),
-    RulesMap(DateTime.now(), "규칙 3", 5),
-    RulesMap(DateTime.now(), "규칙 3", 5),
-    RulesMap(DateTime.now(), "규칙 3", 5),
-    RulesMap(DateTime.now(), "규칙 3", 5),
-    RulesMap(DateTime.now(), "규칙 3", 5),
-    RulesMap(DateTime.now(), "규칙 3", 5),
-    RulesMap(DateTime.now(), "규칙 3", 5),
     RulesMap(DateTime.now(), "규칙 4", 5),
+    RulesMap(DateTime.now(), "규칙 5", 5),
+    RulesMap(DateTime.now(), "규칙 6", 5),
+    RulesMap(DateTime.now(), "규칙 10", 5),
   ];
 
+  double deviceWidth = 300;
   @override
   Widget build(BuildContext context) {
-    double deviceWidth = MediaQuery.of(context).size.width;
     // ignore: no_leading_underscores_for_local_identifiers
-    OutlineInputBorder borderStyles() {
-      return OutlineInputBorder(
-          borderSide: const BorderSide(
-            width: 0,
-            style: BorderStyle.none,
-          ),
-          borderRadius: BorderRadius.circular(20));
-    }
 
-    Container line() {
-      return Container(
-        width: deviceWidth,
-        height: 0.2,
-        color: const Color.fromARGB(255, 100, 100, 100),
-      );
-    }
-
-    Card ruleBox(RulesMap rules) {
-      return Card(
-        shape: RoundedRectangleBorder(
-            //<-- SEE HERE
-            side: const BorderSide(
-              color: Color.fromARGB(255, 188, 188, 188),
-            ),
-            borderRadius: BorderRadius.circular(20)),
-        color: const Color.fromARGB(31, 67, 67, 67),
-        margin: const EdgeInsets.symmetric(vertical: 15),
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: SizedBox(
-            width: deviceWidth - 50,
-            child: RichText(
-              overflow: TextOverflow.ellipsis,
-              maxLines: null,
-              text: TextSpan(
-                text: rules.description,
-                style: const TextStyle(
-                  color: Color.fromARGB(255, 210, 210, 210),
-                  height: 1.4,
-                  fontSize: 20.0,
-                ),
-              ),
-            ),
-          ),
-        ),
-      );
-    }
+    deviceWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
       backgroundColor: const Color.fromARGB(31, 188, 188, 188),
@@ -118,6 +69,7 @@ class _MyAppState extends State<MyApp> {
                   fontWeight: FontWeight.w900,
                   fontSize: 30),
             ),
+            const SizedBox(height: 10),
             Column(
               children: [
                 ListView.builder(
@@ -138,7 +90,99 @@ class _MyAppState extends State<MyApp> {
     );
   }
 
-  TextField ruleInput(OutlineInputBorder borderStyles()) {
+  OutlineInputBorder borderStyles() {
+    return OutlineInputBorder(
+        borderSide: const BorderSide(
+          width: 0,
+          style: BorderStyle.none,
+        ),
+        borderRadius: BorderRadius.circular(20));
+  }
+
+  Container line() {
+    return Container(
+      width: deviceWidth,
+      height: 0.2,
+      color: const Color.fromARGB(255, 100, 100, 100),
+    );
+  }
+
+  InkWell ruleBox(RulesMap rules) {
+    return InkWell(
+      onTap: () {
+        detail(rules.description);
+      },
+      child: Card(
+        shape: RoundedRectangleBorder(
+            //<-- SEE HERE
+            side: const BorderSide(
+              color: Color.fromARGB(255, 188, 188, 188),
+            ),
+            borderRadius: BorderRadius.circular(20)),
+        color: const Color.fromARGB(31, 67, 67, 67),
+        margin: const EdgeInsets.symmetric(vertical: 15),
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: SizedBox(
+            width: deviceWidth - 50,
+            child: RichText(
+              overflow: TextOverflow.ellipsis,
+              maxLines: null,
+              text: TextSpan(
+                text: rules.description,
+                style: const TextStyle(
+                  color: Color.fromARGB(255, 210, 210, 210),
+                  height: 1.4,
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void detail(String rule) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            backgroundColor: Colors.black45,
+            content: Text(rule,
+                textAlign: TextAlign.center,
+                style: const TextStyle(color: Colors.white, fontSize: 18)),
+            actions: <Widget>[
+              TextButton(
+                child: Center(
+                  child: Column(
+                    children: [
+                      Container(
+                        width: double.infinity,
+                        height: 1,
+                        color: Colors.white,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const Icon(
+                        Icons.check,
+                        color: Colors.white,
+                        size: 24.0,
+                      ),
+                    ],
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          );
+        });
+  }
+
+  TextField ruleInput(OutlineInputBorder Function() borderStyles) {
     return TextField(
       decoration: InputDecoration(
         hintText: hint,
